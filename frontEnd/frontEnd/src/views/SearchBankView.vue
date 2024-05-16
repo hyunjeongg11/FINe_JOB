737ec4df3f52fb11d7c9665cbf9fd03261b8bcb9
hyunjeongg11 <guswjd4585@gmail.com>
2024-05-16 16:46:29 +0900

    feat: Develop frontend project

---
+++ frontEnd/frontEnd/src/views/SearchBankView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>djaslkds</h1>
+        <kakaoMap />
+    </div>
+</template>
+
+<script setup>
+import kakaoMap from '@/components/kakaoMap.vue';
+</script>
+
+<style  scoped>
+
+</style>
