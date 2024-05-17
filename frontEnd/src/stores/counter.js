fe8a030aaf66b55a97e541faea4ec086fec010bd
rt21hyuk <rt21hyuk@gmail.com>
2024-05-17 16:18:22 +0900

    feat: Update FreeBoardView.vue

--- frontEnd/src/stores/counter.js
+++ frontEnd/src/stores/counter.js
@@ -1,7 +1,20 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios'
 
 export const useCounterStore = defineStore('counter', () => {
-
-  return {  }
-})
+  // const freeArticles = ref([])
+  // const FREE_BOARD_API_URL = 'http://127.0.0.1:8000'
+  // const getFreeBoards = function() {
+  //   axios({
+  //     method: 'get',
+  //     url: `${API_URL}/api/v1/boards/free/`,
+  //   })
+  //     .then(res => {
+  //       console.log(res)
+  //       console.log(res.data)
+  //     })
+  //     .catch(err => console.log(err))
+  // }
+  // return { freeArticles, FREE_BOARD_API_URL, getFreeBoards }
+}, {persist: true})
