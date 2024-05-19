e640fe00e0857abe7c1b74427d8d3015803d3dd5
rt21hyuk <rt21hyuk@gmail.com>
2024-05-20 08:43:33 +0900

    fix: Modify TodayLuck feature

--- frontEnd/src/components/TodayLuck.vue
+++ frontEnd/src/components/TodayLuck.vue
@@ -2,11 +2,12 @@
     <div>
       <h2>오늘의 운세</h2>
       <div v-if="userStore.token">
-        <div v-if="todayLuck">
-          <p>{{ todayLuck }}</p>
+        <div v-if="todayLuckTitle">
+          <p>{{ todayLuckTitle }}</p>
+          <p>{{ todayLuckContent }}</p>
         </div>
         <div v-else>
-          <p >오늘의 운세를 확인하세요.</p>
+          <p>오늘의 운세를 확인하세요.</p>
           <button @click="getTodayLuck">운세 보기</button>
         </div>
       </div>
@@ -22,7 +23,8 @@
   import { userCheckStore } from '@/stores/usercheck'
   const store = useBoardStore()
   const userStore = userCheckStore()
-  const todayLuck = computed(() => store.todayLuck.content)
+  const todayLuckTitle = computed(() => store.todayLuck.title)
+  const todayLuckContent = computed(() => store.todayLuck.content)
   
   const getTodayLuck = function() {
     store.getTodayLuck()
