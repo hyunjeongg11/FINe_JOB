9ce9cf9f0808914e7ea3b2fc9fba6dd37284fcd2
rt21hyuk <rt21hyuk@gmail.com>
2024-05-20 16:35:45 +0900

    feat: Update MainPage Layout

--- frontEnd/src/components/RecommendJobList.vue
+++ frontEnd/src/components/RecommendJobList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2>인기있는 적금 상품</h2>
+    <h2>추천 공고</h2>
   </div>
 </template>
 
--- frontEnd/src/views/MainPageView.vue
+++ frontEnd/src/views/MainPageView.vue
@@ -4,14 +4,23 @@
       <CurrencyConverter />
     </div>
     <div class="popular-products">
-      <PopularDepositList />
-      <PopularSavingList />
-    </div>
-    <div class="today-luck">
-      <TodayLuck />
+      <div class="popular-deposit">
+        <PopularDepositList />
+      </div>
+      <div class="popular-saving">
+        <PopularSavingList />
+      </div>
     </div>
     <div class="news">
       <NaverNews />
+    </div>
+    <div class="today-info">
+      <div class="today-luck">
+        <TodayLuck />
+      </div>
+      <div class="recommend-job">
+        <RecommendJobList />
+      </div>
     </div>
     <div class="boards">
       <div class="free-board">
@@ -32,45 +41,94 @@ import NaverNews from '@/components/NaverNews.vue'
 import FreeBoardList from '@/components/FreeBoardList.vue'
 import AgeBoardList from '@/components/AgeBoardList.vue'
 import CurrencyConverter from '@/components/CurrencyConverter.vue'
+import RecommendJobList from '@/components/RecommendJobList.vue'
 </script>
 
 <style scoped>
 .main-container {
   display: grid;
   grid-template-areas:
-    "currency-converter popular-products login-box"
-    "news news today-luck"
-    "boards boards boards";
-  grid-gap: 10px;
-  padding: 20px;
+    "currency-converter popular-products"
+    "news today-info"
+    "boards boards";
+  grid-template-columns: 1fr 1fr;
+  gap: 10px;
+}
+
+.main-container > div {
+  /* border: 1px solid black; */
+  padding: 10px;
 }
 
 .currency-converter {
   grid-area: currency-converter;
+  width: 85%;
+  margin-left: 10%;
 }
 
 .popular-products {
   grid-area: popular-products;
   display: flex;
-  justify-content: space-between;
-}
-
-
-.today-luck {
-  grid-area: today-luck;
+  justify-content: space-around;
+  width: 90%;
 }
 
 .news {
   grid-area: news;
+  width: 85%;
+  margin-left: 10%;
+
+}
+
+.today-info {
+  grid-area: today-info;
+  width: 90%;
+  /* display: flex; */
+  justify-content: space-around;
+}
+
+.today-info > div {
+  border: 1px solid black;
+}
+
+.today-luck {
+  order: 1; /* Set the order of TodayLuck to 1 */
+}
+
+.recommend-job {
+  order: 2; /* Set the order of RecommendJobList to 2 */
 }
 
 .boards {
   grid-area: boards;
   display: flex;
   justify-content: space-between;
+  width: 90%;
+  margin-left: 5%;
+}
+
+.popular-products > div {
+  border: 1px solid black;
+}
+
+.boards > div {
+  border: 1px solid black;
 }
 
 .free-board, .age-board {
+  width: 47%;
+}
+
+.today-luck {
+  height: 30%;
+  margin-bottom: 5%;
+}
+
+.recommend-job {
+  height: 65%;
+}
+
+.popular-saving, .popular-deposit {
   width: 48%;
 }
 </style>
