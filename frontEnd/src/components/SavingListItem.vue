e0d3057d4de485a92191c798aad9744937286aa1
hyunjeongg11 <guswjd4585@gmail.com>
2024-05-21 12:18:24 +0900

    feat: Losted file comeback

---
+++ frontEnd/src/components/SavingListItem.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-6 mb-4" v-for="saving in [props.saving]" :key="saving.id">
+        <div class="card" @click="goDetail(saving.fin_prdt_cd, saving.id)">
+          <img :src="`/assets/banks/${saving.kor_co_nm}.png`" class="card-img-top" alt="Bank Logo">
+          <div class="card-body">
+            <p class="card-text">{{ saving.join_way }}</p>
+            <h3 class="card-text">{{ saving.fin_prdt_nm }}</h3>
+            <h5 class="card-text">최대 우대금리: <strong>{{ calculateMaxRate(saving.saving_options).maxRate }}</strong>%</h5>
+            <p class="card-text">기간: {{ calculateMaxRate(saving.saving_options).maxTerm }}개월</p>
+            <button class="btn btn-primary">상세보기</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import { defineProps } from 'vue'
+
+const router = useRouter()
+const props = defineProps({
+  saving: Object
+})
+
+const goDetail = function (bank, id) {
+  router.push({ name: 'savingdetail', params: { fin_prdt_cd: bank, id: id } })
+}
+
+const calculateMaxRate = (savingOptions) => {
+  let maxRate = 0
+  let maxTerm = 0
+
+  for (const option of savingOptions) {
+    if (option.intr_rate > maxRate) {
+      maxRate = option.intr_rate
+      maxTerm = option.save_trm
+    }
+  }
+  return { maxRate, maxTerm }
+}
+</script>
+
+<style scoped>
+.card {
+  margin: 20px;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+  width: 500px; 
+  cursor: pointer;
+}
+img {
+  padding: 50px;
+  border: rgba(173, 170, 170, 0.555) 2px solid;
+}
+.card-body {
+  border: rgba(173, 170, 170, 0.555) 2px solid;
+}
+.card-img-top {
+  width: 100%;
+  height: 15%;
+  object-fit: cover;
+}
+.card-body {
+  padding: 15px;
+}
+.btn-primary {
+  margin-top: 10px;
+  background-color: rgba(113, 166, 201, 0.856);
+  border: none;
+}
+h3 {
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+</style>
