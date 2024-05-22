e6754b6ccf3afb03623904fa514cd58e8d2f78f0
rt21hyuk <rt21hyuk@gmail.com>
2024-05-23 08:51:04 +0900

    fix: Modify Intro Page

--- frontEnd/src/views/IntroView.vue
+++ frontEnd/src/views/IntroView.vue
@@ -1,39 +1,47 @@
 <template>
 	<div class="container">
-		<img class="intro-image-gif" src="/assets/intro/intro1.gif" alt="인트로 이미지 1">
-		<img class="intro-image" src="/assets/carousel/2.jpg" alt="인트로 이미지 2">
-		<img class="intro-image" src="/assets/carousel/3.jpg" alt="인트로 이미지 3">
-		<img class="intro-image" src="/assets/carousel/4.jpg" alt="인트로 이미지 4">
+		<img :src="gifSrc" class="intro-image-gif" alt="인트로 1">
+		<img class="intro-image" src="/assets/carousel/2.jpg" alt="인트로 2">
+		<img class="intro-image" src="/assets/carousel/3.jpg" alt="인트로 3">
+		<img class="intro-image" src="/assets/carousel/4.jpg" alt="인트로 4">
 	</div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from 'vue'
+
+const gifSrc = ref('')
 
 onMounted(() => {
+	resetGif()
 	setTimeout(() => {
 		showImages();
 	}, 300); // 0.3초 뒤에 이미지 보이기 함수 호출
-	window.addEventListener('scroll', showImages);
-});
+	window.addEventListener('scroll', showImages)
+})
 
 const showImages = () => {
-	const images = document.querySelectorAll('.intro-image');
+	const images = document.querySelectorAll('.intro-image')
 	images.forEach((image) => {
 		if (isElementInViewport(image)) {
-			image.classList.add('show');
+			image.classList.add('show')
 		}
-	});
-};
+	})
+}
+
+const resetGif = () => {
+	const timestamp = new Date().getTime()
+	gifSrc.value = `/assets/intro/intro1.gif?${timestamp}`
+}
 
 const isElementInViewport = (el) => {
-	const rect = el.getBoundingClientRect();
-	const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-	const windowWidth = window.innerWidth || document.documentElement.clientWidth;
-	const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
-	const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
-	return (vertInView && horInView);
-};
+	const rect = el.getBoundingClientRect()
+	const windowHeight = window.innerHeight || document.documentElement.clientHeight
+	const windowWidth = window.innerWidth || document.documentElement.clientWidth
+	const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0)
+	const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0)
+	return (vertInView && horInView)
+}
 </script>
 
 <style scoped>
